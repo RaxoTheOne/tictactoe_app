43748439eb0ec5e73d4c61bcb3db438106defe57
RaxoTheOne <traxon.bg@gmail.com>
2024-02-24 13:42:13 +0100

    tictactoe project added

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,152 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TicTacToeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TicTacToeApp extends StatelessWidget {
+  const TicTacToeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Tic Tac Toe',
+      home: TicTacToeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class TicTacToeScreen extends StatefulWidget {
+  const TicTacToeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TicTacToeScreenState createState() => _TicTacToeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TicTacToeScreenState extends State<TicTacToeScreen> {
+  List<List<String>> _board = List.generate(3, (_) => List.filled(3, ''));
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  bool _isPlayer1Turn = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Tic Tac Toe'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _isPlayer1Turn ? 'Player 1 Turn' : 'Player 2 Turn',
+              style: const TextStyle(fontSize: 24.0),
+            ),
+            const SizedBox(height: 20.0),
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: 9,
+              itemBuilder: (BuildContext context, int index) {
+                int row = index ~/ 3;
+                int col = index % 3;
+                return GestureDetector(
+                  onTap: () {
+                    _onTilePressed(row, col);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Center(
+                      child: Text(
+                        _board[row][col],
+                        style: const TextStyle(fontSize: 48.0),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _resetBoard,
+              child: const Text('Reset'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  void _onTilePressed(int row, int col) {
+    if (_board[row][col] == '') {
+      setState(() {
+        _board[row][col] = _isPlayer1Turn ? 'X' : 'O';
+        _isPlayer1Turn = !_isPlayer1Turn;
+      });
+      _checkWinner(row, col);
+    }
+  }
+
+  void _checkWinner(int row, int col) {
+    String player = _board[row][col];
+    bool won = false;
+
+    // Check row
+    for (int i = 0; i < 3; i++) {
+      if (_board[row][i] != player) break;
+      if (i == 2) won = true;
+    }
+
+    // Check column
+    for (int i = 0; i < 3; i++) {
+      if (_board[i][col] != player) break;
+      if (i == 2) won = true;
+    }
+
+    // Check diagonal
+    if (row == col) {
+      for (int i = 0; i < 3; i++) {
+        if (_board[i][i] != player) break;
+        if (i == 2) won = true;
+      }
+    }
+
+    // Check anti-diagonal
+    if (row + col == 2) {
+      for (int i = 0; i < 3; i++) {
+        if (_board[i][2 - i] != player) break;
+        if (i == 2) won = true;
+      }
+    }
+
+    if (won) {
+      showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: const Text('Winner'),
+          content: Text('Player $player won!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _resetBoard();
+              },
+              child: const Text('Play Again'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void _resetBoard() {
+    setState(() {
+      _board = List.generate(3, (_) => List.filled(3, ''));
+      _isPlayer1Turn = true;
+    });
   }
 }
