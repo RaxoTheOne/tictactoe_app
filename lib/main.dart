d562e8f94ce2dfaecbfb4c42402d4ac1f43a77b5
RaxoTheOne <traxon.bg@gmail.com>
2024-05-09 12:18:50 +0200

    changed appdelegate.swift debugShowCheckedModeBanner added with false

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class TicTacToeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tic Tac Toe',
       home: TicTacToeScreen(),
     );
